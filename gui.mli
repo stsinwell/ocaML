ed8f10cff634876f2aefb42ca1affb2a2746dd88
stsinwell <sas575@cornell.edu>
2017-12-06 17:27:16 -0500

    gogogogogooooo matrixixixix

--- gui.mli
+++ gui.mli
@@ -1,10 +1,11 @@
 open GMain
 open GMisc
+open Images
 (* open Matrix *)
 
 (* [to_matrix img] converts the input [img] to a matrix with entries between
  * 0 and 1 to be used by the neural network. *)
-val to_matrix: GDraw.drawable -> float list list
+val to_matrix: Bmp.bmp -> float list 
 
 (* [classify] processes a click of the classify button.
  * It takes the image in the drawing pane, converts it to a matrix,
