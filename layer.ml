28b4bde2f5bbf9750615078cf63077ec5cfd0b82
Your Name <ll698@cornell.edu>
2017-12-06 21:39:20 -0500

    pls

--- layer.ml
+++ layer.ml
@@ -15,3 +15,29 @@ let new_layer (m: int) (n: int) (a1: Actv.t) = {
     w = mat_random n m;
     b = mat_random n 1
   }
+(*
+- l is layer we are updating
+- a is activation of weights for some input
+- g is backprop gradients for some input
+*)
+let update_w_and_b (l: t) (a: matrix) (g: matrix) =
+    (*bp - a vector of same dimension 
+    as g with a hyperparameter for gradient updates*)
+    let bp = 
+        let const = (0.2) *. float_of_int (Mat.dim1 g) in
+        Mat.random (Mat.dim1 g) 1 ~from:(const) ~range:(0.0) in
+
+    
+
+    let new_w w a g = 
+      Mat.Add w (Mat.Neg (Mat.Mul bp 
+
+
+
+
+
+let new_weight e f b l =
+  l.weight <<<->>> ((e /. (scale b)) <*>>> (f <<*|>> b))
+
+let new_bias e b l =
+  l.bias <<->> ((e /. (scale b)) <*>> b)
--- model.ml
+++ model.ml
@@ -2,11 +2,18 @@ open Lacaml.S
 open Layer
 open Matrix
 open Actv
+open Loss
 
 type matrix = Mat.t
+
 type layer = Layer.t
+
 type model = layer list
 
+type network  = {
+    model : model;
+    loss : Loss.t
+  }
 
 let get_category m = failwith "sdfhdsfds"
 
@@ -23,35 +30,59 @@ let propagate (m: model) (input: matrix) =
                     store_weights t out (out::wl)
         in
     store_weights m input []
-
-    (* List.fold_left
-    (fun x layer -> let layer_out = layer.a.f (Mat.add (gemm layer.w x) layer.b) in
-
-                    ) input m *)
-
-let backpropagate (m: model) (input: matrix) (l: matrix) =
-  failwith "clkdsf"
-    (* let rev_net = List.rev m in
-    let layer_update l l' c i =
-        Mat.mul (gemm l.w c ~transa:`T) (l.a.f' i) in
-
+        
+let backpropagate (m: model) (input: matrix) (l: matrix) = 
+    let rev_net = List.rev m in
+    let layer_update l l' c i = 
+        Mat.mul (gemm l.w c ~transa:`T) (l.a.f' i.w) in
+        
     (*updates *)
     (*
     - a is the weight list from running input through model
     - w is the weights of the model *)
-    let network_update (a: model) (l:matrix) =
-        let rec store_weights (a: model)  w l wl =
+    let network_update (a: model) (l:matrix) = 
+        let rec store_weights (a: model)  w l wl = 
+
             (*CHANGE*)
             (*
-            - wl is the accumulator that collects
-              the result of applying layer_update*)
+            - wl is the accumulator that collects 
+              the result of applying layer_update
+              *)
             match (a, w) with
             | (ha1::ha2::ta, hl1::hl2::tl) ->
                 let out = layer_update hl2 hl1 l ha2
                 in
                 store_weights (ha2::ta) (hl2::tl) out (out::wl)
             | _ -> wl in
-        let wl = store_weights a rev_net l [] in
-        List.rev (l::(List.rev wl)) in
+        let wl = store_weights a rev_net l [] in 
+        List.rev (l::(List.rev wl)) in 
+
+    network_update m l
+
+
+let full_pass (n: network) (x: matrix) (y: matrix) = 
+    let forward = propagate n.model x in
+    let cost = n.loss.f' x y in
+    let b_net = backpropagate n.model x in
+
+    let rec helper f b l r = 
+        match (f, b, l) with 
+                | (hf::tf, hb::tb, hl::tl) ->
+                let new_layer =  
+                    {   
+                        a = hl.a;
+                        w = new_weight n.eta hf hb hl;
+                        b = new_bias n.eta hb hl;
+                    }
+                in
+                single_aux tf tb tl (layer::r)
+            | _ -> r
+
+
+
+
+
+
+
 
     network_update m l *)
