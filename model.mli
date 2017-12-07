55616a261c8bb044f793b39be3d3f5a882a5cb20
stsinwell <sas575@cornell.edu>
2017-12-07 15:19:06 -0500

    finished writing specs

--- model.mli
+++ model.mli
@@ -7,10 +7,11 @@ type matrix = Matrix.t
 type layer = Layer.t
 type loss = Loss.t
 
-(* [model] represents the entire neural network, which is a
- * list of layers. *)
+(* [model] represents a list of layers. *)
 type model = layer list
 
+(* [network] represents the entire neural network, which includes a model
+ * and a loss function. *)
 type network  = {
   model : model;
   loss : loss
@@ -27,21 +28,23 @@ val propagate: model -> matrix -> matrix list
  * entire model's output matrix [m2]. *)
 val backpropagate: model -> matrix list -> matrix -> matrix list
 
-(* [get_category m] is the index of the max value in the probability
- * distribution given by matrix [m].
- * requires: [m] is a vector. *)
-(* val get_category: matrix -> int *)
+(* [full_pass n x y] performs gradient update on the model weights and biases
+ * by propagating some input, calculating the gradient for the output loss,
+ * and backpropagating the gradient to update the weights and biases. It
+ * returns the network updated with these weights and biases. [x] is the
+ * data point, and [y] is that data point's label. *)
+val full_pass : network -> matrix -> matrix -> network
 
-val full_pass: network -> matrix -> matrix -> network
-
-
+(* [train n x steps epoch ?id] samples over the inputted dataset [x] and updates
+ * the network [n] iteratively in memory. Its output is the network and a list
+ * of saved files that enable later loading of the network. The network iterates
+ * [epoch] times, where each epoch consists of a number of [steps]. *)
 val train: network -> matrix -> int -> int -> ?id:string -> unit ->
   (network * ((string * string) list))
 
 (* [infer n m] is the index of the max value in the probability
  * distribution given by matrix [m].
  * requires: [m] is a vector. *)
-(* val get_category: matrix -> int *)
 val infer: (string * string) list -> string -> int
 
 (* [save_m m id] saves the weights and biases of all the layers in model [m] as
