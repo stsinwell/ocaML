0ea568685e85afab2df3684efec9ed2ca6b7b2d4
kp483 <kp483@cornell.edu>
2017-12-07 22:09:24 -0500

    is this entire thing wrong

--- visualisation.ml
+++ visualisation.ml
@@ -1,13 +1,12 @@
 open GMain
 open GMisc
 open Graphics
-(* open Matrix *)
-open Lacaml.S
+open Lacaml.D
+open Model
 
 (*[fill_color node] sets the current color according to the  
  * weight (value) of [node]*)
  let fill_color node =
-  (*specify color to fill in node with according to node's weight*)
   let shade = 
   if node < 0.1 
   then 0xffffff
@@ -32,10 +31,37 @@ open Lacaml.S
   else 0x000000 in 
   set_color shade
 
+(* returns: [take_rev n xs acc] is [lst1 @ acc], where [lst] is
+ *   the first [n] elements of [xs] (or just [xs] if [xs] has 
+ *   fewer than [n] elements) in reverse order. Taken from 3110 lab. 
+ * requires: [n >= 0] *)
+ let rec take_rev n xs acc = 
+  if n = 0 then acc else match xs with
+    | [] -> acc
+    | x::xs' -> take_rev (n-1) xs' (x::acc)
+
+(* returns:  [take n lst] is the first [n] elements of [lst], or
+ *   just [lst] if [lst] has fewer than [n] elements. Taken from
+ *   3110 lab.
+ * requires: [n >= 0]
+ *)
+let take n lst =
+  take_rev n lst [] |> List.rev
+
+(* returns:  [drop n lst] is all but the first [n] elements of [lst],
+ *   or just [[]] if [lst] has fewer than [n] elements. Takne from 
+ *   3110 lab.
+ * requires: [n >= 0]
+ *)
+ let rec drop n lst =
+  if n = 0 then lst else match lst with
+    | [] -> []
+    | x::xs -> drop (n-1) xs
+
 (*[draw_circles node_lst spacing x_pos y_pos] draws the circles
  * for a single layer of nodes *)
 let rec draw_circles (node_lst : float list) spacing x_pos y_pos =  
-match List.rev node_lst with 
+match node_lst with 
 | [] -> draw_circle 0 0 0 
 | h :: t -> set_color 0x000000;
            draw_circle x_pos y_pos 10;
@@ -43,37 +69,60 @@ match List.rev node_lst with
            fill_circle x_pos y_pos 10;
            draw_circles t spacing x_pos (y_pos + spacing)
 
-
 (*[insert_layer] draws the nodes of a single layer
  * onto the window *)
-let insert_layer (node_lst: float list) x_pos =
+let insert_layer (node_lst: float list) x_pos=
   let num_nodes = List.length node_lst in
   let offset = 50 in
   let spacing = (size_y() - 2 * offset) / num_nodes in
   draw_circles node_lst spacing x_pos offset
 
-  (*[get_startpoints layers_lst] returns a list of the 
-   * coordinates of each node for each of the n layers in 
-   * [layers_lst] except for the nth layer. 
-   * Helper function to [connect_layers].*)
-let get_startpoints layers_lst = 
-  failwith "Unimplemented"
+let get_points_helper node_lst x_pos =
+ let num_nodes = List.length node_lst in 
+ let offset = 50 in 
+ let spacing = (size_y() - 2 * offset) / num_nodes in
+ let rec helper node_lst offset' =
+  match node_lst with
+  | [] -> [] 
+  | h :: t -> (x_pos, offset') :: helper t (offset' + spacing)
+  in
+  helper node_lst offset
 
-  (*[get_endpoints layers_lst] returns a list of the 
-   * coordinates of each node or each of the n layers in 
-   * [layers_lst] except for the 0th layer. 
-   * Helper function to [connect_layers].*)
-let get_endpoints layers_lst =
-  failwith "Unimplemented"
+
+ let rec connect_one_node s e =
+  match s with
+  | (x,y) :: t -> List.map (fun (x', y') -> moveto x y; lineto x' y') e; connect_one_node t e;
+  | _ -> () 
+
+(* [match_points_node] draws a line from the node at position ([sx],[sy])
+ * to each of the nodes in the list [end_lst]*)
+let rec match_points points =
+  match points with
+  | h :: m :: t -> connect_one_node h m; match_points (m :: t) 
+  | h :: t :: [] -> connect_one_node h t
+  | _ -> ()
+
+  (*[get_startendpoints layers_lst] returns a list of the 
+   * coordinates of each node for each of the n layers in 
+   * [layers_lst]. Helper function to [connect_layers].*)
+let rec get_startendpoints layers_lst = 
+  let num_layers = List.length layers_lst in
+  let offset = 50 in 
+  let x_spacing = (size_x() - 100) / num_layers in 
+  let rec helper layers_lst offset' =
+    match layers_lst with
+    | [] -> []
+    | h :: t -> get_points_helper h offset' ::  (helper t (offset' + x_spacing))
+  in 
+  helper layers_lst offset
 
 (*[connect_layers] draws lines symbolizing
  * all of the different possible paths through
  * the neural network *)
-let connect_layers (layers_lst : Mat.t list) = 
-  let startpoints = get_startpoints layers_lst in
-  let endpoints = get_endpoints layers_lst in 
-  set_color 0x000000
-
+let connect_layers (layers_lst : float list list) = 
+  let startendpoints = get_startendpoints layers_lst in
+  set_color 0x000000;
+  match_points startendpoints
 
 let arrange_layers (layer_lst : float list list ) =
   (*let transformed = to_list layer_lst in*)
@@ -93,11 +142,17 @@ let arrange_layers (layer_lst : float list list ) =
   in
   insert_layers transformed offset
 
+let get_input =
+  propagate 
+  
+
 let main () =
   open_graph " 1000x500";
   set_window_title "Network Visualization";
-  let sample = [[0.1;0.5;1.0]; [1.0;0.8;0.9]; [0.1;0.2]] in 
+  let sample = [[0.1;0.5;1.0]; [1.0;0.5;0.9]; [0.1;0.2]] in 
+  connect_layers sample;
   arrange_layers sample;
+  
   
   (*wait for user input//keep window running*)
   ignore (Graphics.read_key ())
