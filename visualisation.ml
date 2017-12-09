f951f4b7e43492275214818ae0ac3ffb0a026941
kp483 <kp483@cornell.edu>
2017-12-08 23:08:55 -0500

    final

--- visualisation.ml
+++ visualisation.ml
@@ -2,150 +2,190 @@ open GMain
 open GMisc
 open Graphics
 open Lacaml.D
-open Train_mnist
 open Model
-include Matrix
 
-
-(*[fill_color neuron] sets the current color according to the  
- * activation (value) of [neuron]*)
- let fill_color neuron =
+(*[fill_color node] sets the current color according to the  
+ * weight (value) of [node]*)
+ let fill_color node =
   let shade = 
-  if neuron < 0.1 
+  if node < 0.1 
   then 0xffffff
-  else if (neuron >= 0.1) && (neuron < 0.2) 
+  else if (node >= 0.1) && (node < 0.2) 
   then 0xe6e6e6 
-  else if (neuron >= 0.2) && (neuron < 0.3)
+  else if (node >= 0.2) && (node < 0.3)
   then 0xcccccc
-  else if (neuron >= 0.3) && (neuron < 0.4)
+  else if (node >= 0.3) && (node < 0.4)
   then 0xb3b3b3
-  else if (neuron >= 0.4) && (neuron < 0.5)
+  else if (node >= 0.4) && (node < 0.5)
   then 0x999999
-  else if (neuron >= 0.5) && (neuron < 0.6)
+  else if (node >= 0.5) && (node < 0.6)
   then 0x808080
-  else if (neuron >= 0.6) && (neuron < 0.7)
+  else if (node >= 0.6) && (node < 0.7)
   then 0x666666
-  else if (neuron >= 0.7) && (neuron < 0.8)
+  else if (node >= 0.7) && (node < 0.8)
   then 0x4d4d4d
-  else if (neuron >= 0.8) && (neuron < 0.9)
+  else if (node >= 0.8) && (node < 0.9)
   then 0x333333
-  else if (neuron >= 0.9) && (neuron < 1.0)
+  else if (node >= 0.9) && (node < 1.0)
   then 0x1a1a1a
   else 0x000000 in 
   set_color shade
 
 (*[line_color weight] sets the current color according to the
  * value of [weight] *)
-let line_color weight = 
+ let line_color weight = 
   let shade = 
-  if (weight >= -1.) && (weight < (-0.8))
+  if (weight >= 0.0) && (weight < (0.1))
   then 0xff0000
-  else if (weight >= -0.8) && (weight < (-0.6))
+  else if (weight >= 0.1) && (weight < (0.2))
   then 0xff3333
-  else if (weight >= -0.6) && (weight < (-0.4))
+  else if (weight >= 0.2) && (weight < 0.3)
   then 0xff6666
-  else if (weight >= (-0.4)) && (weight < (-0.2))
+  else if (weight >= 0.3) && (weight < (0.4))
   then 0xff9999
-  else if (weight >= (-0.2)) && (weight < (0.0))
+  else if (weight >= (0.4)) && (weight < (0.5))
   then 0xffcccc
-  else if (weight >= (0.0)) && (weight < (0.2))
+  else if (weight >= (0.5)) && (weight < (0.6))
   then 0xffffff
-  else if (weight >= (0.2)) && (weight < (0.4))
+  else if (weight >= (0.6)) && (weight < (0.7))
   then 0xe6ffcc
-  else if (weight >= 0.4) && (weight < 0.6)
+  else if (weight >= 0.7) && (weight < 0.8)
   then 0xccff99
-  else if (weight >= 0.6) && (weight < 0.8)
+  else if (weight >= 0.8) && (weight < 0.9)
   then 0xb3ff66
-  else if (weight >= 0.8) && (weight < 1.0)
+  else if (weight >= 0.9) && (weight < 1.0)
   then 0x99ff33
   else 
   0x80ff00 in
   set_color shade
 
-let rec match_neurons start_lst x_space init_sy_space init_ey_space sy_space ey_space = 
-    match start_lst with 
-    | [] -> ()
-    | h :: t -> moveto x_space init_sy_space; 
-               (* fill_color h; 
-               fill_circle x_space (init_sy_space) 5;  *)
-               line_color h; 
-               lineto ( x_space * 2 - 50) init_ey_space;
-               match_neurons t x_space (init_sy_space + sy_space) (init_ey_space + ey_space) sy_space ey_space
-  
+(* returns: [take_rev n xs acc] is [lst1 @ acc], where [lst] is
+ *   the first [n] elements of [xs] (or just [xs] if [xs] has 
+ *   fewer than [n] elements) in reverse order. Taken from 3110 lab. 
+ * requires: [n >= 0] *)
+ let rec take_rev n xs acc = 
+  if n = 0 then acc else match xs with
+    | [] -> acc
+    | x::xs' -> take_rev (n-1) xs' (x::acc)
+
+(* returns:  [take n lst] is the first [n] elements of [lst], or
+ *   just [lst] if [lst] has fewer than [n] elements. Taken from
+ *   3110 lab.
+ * requires: [n >= 0]
+ *)
+let take n lst =
+  take_rev n lst [] |> List.rev
+
+(* returns:  [drop n lst] is all but the first [n] elements of [lst],
+ *   or just [[]] if [lst] has fewer than [n] elements. Takne from 
+ *   3110 lab.
+ * requires: [n >= 0]
+ *)
+ let rec drop n lst =
+  if n = 0 then lst else match lst with
+    | [] -> []
+    | x::xs -> drop (n-1) xs
+
+(*[draw_circles node_lst spacing x_pos y_pos] draws the circles
+ * for a single layer of nodes *)
+let rec draw_circles (node_lst : float list) spacing x_pos y_pos =  
+match node_lst with 
+| [] -> draw_circle 0 0 0 
+| h :: t -> set_color 0x000000;
+           draw_circle x_pos y_pos 10;
+           fill_color h;
+           fill_circle x_pos y_pos 10;
+           draw_circles t spacing x_pos (y_pos + spacing)
+
+(*[insert_layer] draws the nodes of a single layer
+ * onto the window *)
+let insert_layer (node_lst: float list) x_pos=
+  let num_nodes = List.length node_lst in
+  let offset = 50 in
+  let spacing = (size_y() - 2 * offset) / num_nodes in
+  draw_circles node_lst spacing x_pos offset
+
+(*[get_points_helper] returns a list of the
+ * [x],[y] positions in [node_lst] where [x] is always
+ * [x_pos] *)
+let get_points_helper node_lst x_pos =
+ let num_nodes = List.length node_lst in 
+ let offset = 50 in 
+ let spacing = (size_y() - 2 * offset) / num_nodes in
+ let rec helper node_lst offset' =
+  match node_lst with
+  | [] -> [] 
+  | h :: t -> (x_pos, offset') :: helper t (offset' + spacing)
+  in
+  helper node_lst offset
+
+(*[connect_one_node s e] connects a single neuron in layer [s] to 
+ * each neuron in layer [e] by drawing a line between the neurons.*)
+ let rec connect_one_node s e =
+  match s with
+  | (x,y) :: t -> List.map (fun (x', y') -> moveto x y; 
+                                        line_color (Random.float 1.0);
+                                         lineto x' y') e; 
+                                         connect_one_node t e;
+  | _ -> () 
+
+(* [match_points_node] draws a line from the node at position ([sx],[sy])
+ * to each of the nodes in the list [end_lst]*)
+let rec match_points points =
+  match points with
+  | h :: m :: t -> connect_one_node h m; match_points (m :: t) 
+  | h :: t :: [] -> connect_one_node h t
+  | _ -> ()
+
+  (*[get_startendpoints layers_lst] returns a list of the 
+   * coordinates of each node for each of the n layers in 
+   * [layers_lst]. Helper function to [connect_layers].*)
+let rec get_startendpoints layers_lst = 
+  let num_layers = List.length layers_lst in
+  let offset = 50 in 
+  let x_spacing = (size_x() - 100) / num_layers in 
+  let rec helper layers_lst offset' =
+    match layers_lst with
+    | [] -> []
+    | h :: t -> get_points_helper h offset' ::  (helper t (offset' + x_spacing))
+  in 
+  helper layers_lst offset
 
 (*[connect_layers] draws lines symbolizing
  * all of the different possible paths through
  * the neural network *)
-let connect_layers mat x_spacing =
-  let num_sneurons = List.length (List.hd mat) in   
-  let num_eneurons = List.length mat in 
-  let sy_spacing = (size_y() - 100) / num_sneurons in 
-  let ey_spacing = (size_y() - 100) / num_eneurons in 
-  let rec connect_layers_help mat y_space = 
-    match mat with 
-    | [] -> ()
-    | h :: t -> match_neurons h x_spacing 50 50 sy_spacing ey_spacing; connect_layers_help t (x_spacing * 2)
+let connect_layers (layers_lst : float list list) = 
+  let startendpoints = get_startendpoints layers_lst in
+  set_color 0x000000;
+  match_points startendpoints
+
+
+(*[arrange_layers] determines the spacing for all of the 
+ * layers within the window, and sends for every layer
+ * of neurons to be drawn *)
+let arrange_layers (layer_lst : float list list ) =
+  let num_layers = List.length layer_lst in
+  let offset = 50 in
+  let x_spacing = (size_x() - 100) / num_layers in 
+
+  (*send each layer off to be drawn*)
+  let rec insert_layers (layer_lst: float list list) x_pos = 
+    match layer_lst with
+    | [] -> draw_circle 0 0 0
+    | h :: t -> insert_layer h x_pos; (insert_layers t (x_pos + x_spacing))
   in
-  connect_layers_help mat 50
-
-(*[get_matrices network] gets each matrix within
- * [network] and sends it to be drawn in the GUI.*)
- let rec get_dim network =
-  match network with 
-  | [] -> []
-  | (h :: _) :: t -> List.length h :: get_dim t
-  | [] :: t -> []
-
-let draw_neurons num_neurons x_spacing = 
-  let offset = 50 in 
-  let y_spacing = (size_y() - 100) / num_neurons in
-  for i = 1 to num_neurons do begin
-    draw_circle x_spacing (offset + (i-1) * y_spacing) 5;
-    set_color black;
-    fill_circle x_spacing (offset + (i-1)* y_spacing) 5
-  end
-done
-
-let build_nodes network = 
-  let num_layers = List.length network in 
-  let layer_lens = get_dim network in 
-  
-  let offset = 50 in 
-  let x_spacing = (size_x() - 100) / num_layers in
-  let rec send_matrix network = 
-    match network with 
-    | [] -> ()
-    | h :: t -> connect_layers h x_spacing; send_matrix t
-  in
-  send_matrix network;
-
-  let rec build_layer layer_lens x_pos =
-    match layer_lens with
-    | [] -> ()
-    | h :: t -> draw_neurons h x_pos; build_layer t (x_pos + x_spacing)
-  in
-  build_layer layer_lens offset
-
-let get_input () = 
-  failwith "Unimplemented"
+  insert_layers layer_lst offset
 
 let main () =
-  open_graph " 1100x700";
+  open_graph " 1000x500";
   set_window_title "Network Visualization";
-  let sample = [
-                 [[0.1;(-0.5);(-1.0);0.4;0.2]; 
-                 [1.0;0.5;0.9;(-0.6);(-0.2);(-0.3)]; 
-                 [0.1;0.2;0.3;0.4;0.5;0.6]] ;
-                (*layer 2 to layer 3*)
-               [[0.3;(-0.15);0.3];
-               [0.4; 0.01; (-0.9)]]
-               ]in 
-  (* let mat = Matrix.load_weights "."^Filename.dir_sep^"matrices"^Filename.dir_sep^"matrix_user.txt" in 
-  build_nodes (propagate (fst Train_mnist.new_net).model mat) *)
-  build_nodes sample;
-  (* connect_layers sample;
+  let sample = [[0.1;0.5;1.0; 0.3; 0.4;0.2;0.8;1.0;0.0]; 
+                [1.0;0.5;0.9;0.3;0.4]; 
+                [1.0;0.2;0.4;0.5;0.4]; 
+                [0.1;0.2]] in 
+  connect_layers sample;
   arrange_layers sample;
-   *)
+  
   
   (*wait for user input//keep window running*)
   ignore (Graphics.read_key ())
@@ -153,5 +193,3 @@ let main () =
 
 (* Run the GUI *)
 let () = main ()
-
-
